be5977cf75297fe757850b8734b83b11e26b309b
Gwynne Raskind <gwynne@darkrainfall.org>
2024-03-02 07:04:57 -0600

    Start minimal update with the manifest

--- Package.swift
+++ Package.swift
@@ -1,18 +1,31 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "mustache",
     platforms: [
-       .macOS(.v10_14)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
     ],
     products: [
         .library(name: "Mustache", targets: ["Mustache"]),
     ],
-    dependencies: [ ],
+    dependencies: [],
     targets: [
         .target(name: "CMustache"),
-        .target(name: "Mustache", dependencies: ["CMustache"]),
-        .testTarget(name: "MustacheTests", dependencies: ["Mustache"]),
+        .target(
+            name: "Mustache",
+            dependencies: [
+                .target(name: "CMustache"),
+            ]
+        ),
+        .testTarget(
+            name: "MustacheTests",
+            dependencies: [
+                .target(name: "Mustache"),
+            ]
+        ),
     ]
 )
